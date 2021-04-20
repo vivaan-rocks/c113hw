126ad00951db01cf1928815f5eab01e51d38788c
vivaan-rocks <79306482+vivaan-rocks@users.noreply.github.com>
2021-04-20 13:17:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//predifined functions of p5 library
+
 function preload(){
 }
 
@@ -14,8 +14,14 @@ function draw(){
     fill(255,0,0)
     stroke(255,0,0)
     circle(50,50,80)
+    circle(590,50,80)
+    circle(50,430,80)
+    circle(590,430,80)
 
     fill(0,120,0)
     stroke(0,128,0)
     rect(90,420,460,20)
+    rect(90,50,460,20)
+    rect(600,90,20,300)
+    rect(30,90,20,300)
 }
